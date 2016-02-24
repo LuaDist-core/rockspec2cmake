f5d47d1895e95a2fa294fc17e96a04269878c4c9
f4rnham <f4rnham@users.noreply.github.com>
2016-02-24 22:54:23 +0100

    Implement conversion for C/CXX module build

--- rockspec2cmake.lua
+++ rockspec2cmake.lua
@@ -16,40 +16,94 @@ local function load_rockspec(filename, env)
     return load_table(str)
 end
 
+local function table_concat(tbl)
+    return table.concat(tbl, " ")
+end
 
 local intro = Template[[
 # Generated Cmake file begin
 cmake_minimum_required(VERSION 3.1)
 
-project(${package_name} CXX)
+project(${package_name} C CXX)
 ]]
 
-local unsupported_platform_check = Template[[
+local unsupported_platform_check = Template [[
 if (${platform})
     message(FATAL_ERROR "Unsupported platform (your platform was explicitly marked as not supported)")
 endif()
 ]]
 
-local supported_platform_check = Template[[
+local supported_platform_check = Template [[
 if (${expr})
     message(FATAL_ERROR "Unsupported platform (your platform is not in list of supported platforms)")
 endif()
 ]]
 
-
-local builtin = [[
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c99 -ggdb3 -O0")
-
-set(SOURCE_FILES mongo-module.c lua-mongoc-client.c lua-mongoc-database.c
-lua-mongoc-collection.c lua-bson.c)
-
-add_library(mongo_module SHARED ${SOURCE_FILES} lua-version-compat.h
-lua-version-compat.c lua-object-generators.c lua-mongo-cursor.c)
-
-target_link_libraries(${CMAKE_PROJECT_NAME} ${LIBMONGOC_LDFLAGS} ${LIBLUA_LDFLAGS})
-set_target_properties (${CMAKE_PROJECT_NAME} PROPERTIES PREFIX "")
-
+local find_libraries = Template [[
+find_library(${lib} ${lib} ${path})
 ]]
+
+local cxx_module = Template [[
+include_directories(${incdirs})
+
+add_library(${name} MODULE ${sources})
+
+${find_libraries}
+target_link_libraries(${name} ${libraries})
+
+add_definitions(${defines})
+]]
+
+local function install_lua_file(name, path)
+    -- print("Install lua file " .. name .. " - " .. path)
+    
+    -- Force install file as name.lua, rename if needed
+end
+
+local function add_cxx_module(module_info)
+    local libraries = ""
+    if module_info.libraries ~= nil then
+        local libdirs = table_concat(module_info.libdirs)
+        for _, lib in pairs(module_info.libraries) do
+            libraries = libraries .. find_libraries:substitute({ lib = lib, path = libdirs })
+        end
+    end
+    
+    print(cxx_module:substitute({ incdirs = table_concat(module_info.incdirs), name = module_info.name,
+        sources = table_concat(module_info.sources), find_libraries = libraries, libraries = table_concat(module_info.libraries),
+        defines = table_concat(module_info.defines) }))
+end
+
+local function process_builtin(rockspec)
+    -- Process per-platform overrides
+    if rockspec.build.platforms ~= nil then
+        for _, platform in ipairs(rockspec.build.platforms) do
+            -- foo
+        end
+    end
+    
+    for name, info in pairs(rockspec.build.modules) do
+        if type(info) == "string" then
+            -- Pathname of Lua file or C source, for modules based on single source file
+            local ext = info:match(".([^.]+)$")
+            if ext == "lua" then
+                install_lua_file(name, info)
+            else
+                add_cxx_module({ name = name, sources = { info } })
+            end
+        elseif type(info) == "table" then
+            -- Two options:
+            -- array of strings - pathnames of C sources
+            -- table - possible fields sources, libraries, defines, incdirs, libdirs
+            if type(info.sources) == "string" then
+                info.sources = { info.sources }
+            end
+            
+            add_cxx_module({ name = name, sources  = info.sources, libraries = info.libraries, 
+                defines = info.defines, incdirs = info.incdirs, libdirs = info.libdirs })
+        end
+    end
+end
 
 if #arg ~= 1 then
     print("Expected one argument...")
@@ -96,24 +150,11 @@ else
         end
 
         if rockspec.build.type == "builtin" then
-            --[[print(subst(builtin,{
-                _escape = ">",
-                pairs = pairs,
-                ipairs = ipairs,
-                package_name = rockspec.package,
-                dependencies = rockspec.dependencies,
-                ext_dependencies = rockspec.external_dependencies
-                })) ]]
-        elseif rockspec.build.type == "make" then
-            print("message(FATAL_ERROR \"Unhandled rockspec build type\"")
+            process_builtin(rockspec)
         elseif rockspec.build.type == "cmake" then
-            print("message(FATAL_ERROR \"Unhandled rockspec build type\"")
-        elseif rockspec.build.type == "command" then
-            print("message(FATAL_ERROR \"Unhandled rockspec build type\"")
-        elseif rockspec.build.type == "none" then
-            print("message(FATAL_ERROR \"Unhandled rockspec build type\"")
+            print("message(FATAL_ERROR \"Rockspec build type is cmake, please use the attached one\")")
         else
-            print("message(FATAL_ERROR \"Unhandled rockspec build type\"")
+            print("message(FATAL_ERROR \"Unhandled rockspec build type\")")
         end
      end
 end
