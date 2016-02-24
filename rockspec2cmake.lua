aab4bb1aac2d6b8895b13e801bd765248b7935b6
f4rnham <f4rnham@users.noreply.github.com>
2016-02-24 18:38:19 +0100

    Use pl.pretty.load to load rockspecs instead of code from luarocks

--- rockspec2cmake.lua
+++ rockspec2cmake.lua
@@ -1,79 +1,18 @@
+local load_table = require 'pl.pretty'.load
 local subst = require 'pl.template'.substitute
 
-
--- luarocks/persist.lua
-
---- Load and run a Lua file in an environment.
--- @param filename string: the name of the file.
--- @param env table: the environment table.
--- @return (true, any) or (nil, string, string): true and the return value
--- of the file, or nil, an error message and an error code ("open", "load"
--- or "run") in case of errors.
-local function run_file(filename, env)
-   local fd, err = io.open(filename)
-   if not fd then
-      return nil, err, "open"
-   end
-   local str, err = fd:read("*a")
-   fd:close()
-   if not str then
-      return nil, err, "open"
-   end
-   str = str:gsub("^#![^\n]*\n", "")
-   local chunk, ran
-   if _VERSION == "Lua 5.1" then -- Lua 5.1
-      chunk, err = loadstring(str, filename)
-      if chunk then
-         setfenv(chunk, env)
-         ran, err = pcall(chunk)
-      end
-   else -- Lua 5.2
-      chunk, err = load(str, filename, "t", env)
-      if chunk then
-         ran, err = pcall(chunk)
-      end
-   end
-   if not chunk then
-      return nil, "Error loading file: "..err, "load"
-   end
-   if not ran then
-      return nil, "Error running file: "..err, "run"
-   end
-   return true, err
-end
-
---- Load a Lua file containing assignments, storing them in a table.
--- The global environment is not propagated to the loaded file.
--- @param filename string: the name of the file.
--- @param tbl table or nil: if given, this table is used to store
--- loaded values.
--- @return (table, table) or (nil, string, string): a table with the file's assignments
--- as fields and set of undefined globals accessed in file,
--- or nil, an error message and an error code ("open"; couldn't open the file,
--- "load"; compile-time error, or "run"; run-time error)
--- in case of errors.
-function load_into_table(filename, tbl)
-   assert(type(filename) == "string")
-   assert(type(tbl) == "table" or not tbl)
-
-   local result = tbl or {}
-   local globals = {}
-   local globals_mt = {
-      __index = function(t, k)
-         globals[k] = true
-      end
-   }
-   local save_mt = getmetatable(result)
-   setmetatable(result, globals_mt)
-   
-   local ok, err, errcode = run_file(filename, result)
-   
-   setmetatable(result, save_mt)
-
-   if not ok then
-      return nil, err, errcode
-   end
-   return result, globals
+local function load_rockspec(filename, env)
+    local fd, err = io.open(filename)
+    if not fd then
+        return nil
+    end
+    local str, err = fd:read("*all")
+    fd:close()
+    if not str then
+        return nil
+    end
+    -- str = str:gsub("^#![^\n]*\n", "")
+    return load_table(str)
 end
 
 
@@ -169,7 +108,7 @@ set_target_properties (${CMAKE_PROJECT_NAME} PROPERTIES PREFIX "")
 if #arg ~= 1 then
     print("Expected one argument...")
 else
-    local rockspec, err = load_into_table(arg[1])
+    local rockspec = load_rockspec(arg[1])
     
     if not rockspec then
         print("Failed to load rockspec file (" .. arg[1] .. ")")
